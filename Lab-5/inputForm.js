c44e7315b4b85a75d1282ed654f6dfd28b772923
Ammar1122s <you@example.com>
2023-03-13 16:24:50 +0500

    inputForm

--- Lab-5/inputForm.js
+++ Lab-5/inputForm.js
@@ -1,24 +1,29 @@
-// var check = document.getElementById("btn")
-// check.addEventListener("click", () => {
-//     console.log("ok")
-// })
+
 
 function check(){
     var emailCheck = document.getElementById("email");
+
+    var passCheck = document.getElementById("pass");
 
     if(!emailCheck.value){
         emailCheck.classList.remove("email1");
         emailCheck.classList.add("email2")
     }
+
+    if(!passCheck.value){
+        passCheck.classList.remove("pass1");
+        passCheck.classList.add("email2");
+    }
 }
 
-// var emailC = document.getElementById("email");
-// emailC.addEventListener("change",onchange) 
 
 function onChange(){
     var emailCheck = document.getElementById("email");
+    var passCheck = document.getElementById("pass");
     emailCheck.classList.remove("email2");
     emailCheck.classList.add("email1")
+    passCheck.classList.remove("email2");
+    passCheck.classList.add("pass1");
 console.log("Change")
  }
 
