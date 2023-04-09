a9cb95a7441267bda2f926a5fa714f03a555ba7d
Ammar1122s <you@example.com>
2023-04-10 00:34:48 +0500

    new

---
+++ web-project/public/navigation.js
@@ -0,0 +1,14 @@
+window.onload = start
+
+function start () {
+const exp_btn = document.getElementById("explore")
+
+console.log(exp_btn)
+
+exp_btn.onclick(onclick_btn)
+
+function onclick_btn(){
+    console.log("oK")
+}
+    
+}
